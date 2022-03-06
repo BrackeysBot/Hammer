b849d773bc56021d7604ed2a19503e7bef1ea26e
Oliver Booth <me@olivr.me>
2022-03-06 11:01:33 +0000

    Add v3 compat UserData/Infraction models for migration

---
+++ Hammer/Data/v3_compat/TemporaryInfractionType.cs
@@ -0,0 +1,8 @@
+﻿namespace Hammer.Data.v3_compat;
+
+[Obsolete("This type exists for migration purposes. Please use Hammer.Data.InfractionType")]
+internal enum TemporaryInfractionType
+{
+    TempBan,
+    TempMute
+}
---
+++ Hammer/Data/v3_compat/UserData.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hammer.Data.v3_compat;
+
+[Obsolete("This type exists for migration purposes. Please use Hammer.Data.Infraction")]
+internal class UserData
+{
+    [JsonPropertyName("id")]
+    public ulong ID { get; set; }
+
+    [JsonPropertyName("temporaryInfractions")]
+    public List<TemporaryInfraction> TemporaryInfractions { get; set; } = new();
+
+    [JsonPropertyName("infractions")]
+    public List<Infraction> Infractions { get; set; } = new();
+
+    [JsonPropertyName("muted")]
+    public bool Muted { get; set; }
+}
