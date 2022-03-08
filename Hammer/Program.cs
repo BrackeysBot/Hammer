206c37bddb0863e4966e34e905fa77642055707b
Oliver Booth <me@olivr.me>
2022-03-08 00:24:38 +0000

    Add StartupService and boilerplater Program host

--- Hammer/Program.cs
+++ Hammer/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(builder => builder.AddJsonFile("config.json", false, true))
+    .ConfigureServices(services =>
+    {
+        services.AddLogging(builder =>
+        {
+            builder.ClearProviders();
+            builder.AddNLog();
+        });
+
+        services.AddSingleton(provider =>
+        {
+            var configuration = new DiscordConfiguration
+            {
+                Token = Environment.GetEnvironmentVariable("BOT_TOKEN"),
+                Intents = DiscordIntents.All,
+                LoggerFactory = new NLogLoggerFactory(),
+                ServiceProvider = provider
+            };
+
+            return new DiscordClient(configuration);
+        });
+
+        services.AddSingleton<ConfigurationService>();
+
+        services.AddHostedService<StartupService>();
+    })
+    .UseConsoleLifetime()
+    .RunConsoleAsync();
---
+++ Hammer/Services/StartupService.cs
@@ -0,0 +1,66 @@
+using DisCatSharp;
+using DisCatSharp.ApplicationCommands;
+using DisCatSharp.CommandsNext;
+using DisCatSharp.Interactivity;
+using DisCatSharp.Interactivity.Extensions;
+using Hammer.CommandModules;
+using Hammer.CommandModules.Rules;
+using Hammer.CommandModules.Staff;
+using Hammer.CommandModules.User;
+using Hammer.Configuration;
+using Hammer.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog;
+
+namespace Hammer.Services;
+
+/// <summary>
+///     Represents a class which performs startup initialization, including creating the database and connecting the Discord
+///     client.
+/// </summary>
+internal sealed class StartupService : BackgroundService
+{
+    private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly DiscordClient _discordClient;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="StartupService" /> class.
+    /// </summary>
+    public StartupService(
+        IServiceProvider serviceProvider,
+        IServiceScopeFactory scopeFactory,
+        DiscordClient discordClient
+    )
+    {
+        _serviceProvider = serviceProvider;
+        _scopeFactory = scopeFactory;
+        _discordClient = discordClient;
+    }
+
+    /// <inheritdoc />
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using IServiceScope scope = _scopeFactory.CreateScope();
+        await using var context = scope.ServiceProvider.GetRequiredService<HammerContext>();
+        await context.Database.EnsureCreatedAsync(stoppingToken);
+
+        Logger.Info($"Registering ApplicationCommandsExtension");
+        ApplicationCommandsExtension applicationCommands = _discordClient.UseApplicationCommands(
+            new ApplicationCommandsConfiguration
+            {
+                ServiceProvider = _serviceProvider,
+            });
+
+        Logger.Info("Registering InteractivityExtension");
+        _discordClient.UseInteractivity(new InteractivityConfiguration());
+
+        Logger.Info("Connecting to Discord");
+        await _discordClient.ConnectAsync();
+
+        await applicationCommands.CleanGlobalCommandsAsync();
+        await applicationCommands.CleanGuildCommandsAsync();
+    }
+}
