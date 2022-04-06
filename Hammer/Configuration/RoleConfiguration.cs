956d2210bbfc8c092b956ce159e918c9452172f8
Oliver Booth <me@olivr.me>
2022-04-06 23:19:04 +0100

    Remove permission roles
    These roles are now handled by the Core plugin

--- Hammer/Configuration/RoleConfiguration.cs
+++ Hammer/Configuration/RoleConfiguration.cs
@@ -8,45 +8,9 @@ namespace Hammer.Configuration;
 internal sealed class RoleConfiguration
 {
     /// <summary>
-    ///     Gets or sets the Administrator role ID.
-    /// </summary>
-    /// <value>The Administrator role ID.</value>
-    [JsonPropertyName("administratorRoleId")]
-    public ulong AdministratorRoleId { get; set; }
-
-    /// <summary>
-    ///     Gets or sets the role ID of the bot developer.
-    /// </summary>
-    /// <value>The bot developer role ID.</value>
-    [JsonPropertyName("developerRoleId")]
-    public ulong DeveloperRoleId { get; set; }
-
-    /// <summary>
-    ///     Gets or sets the Guru role ID.
-    /// </summary>
-    /// <value>The Guru role ID.</value>
-    [JsonPropertyName("guruRoleId")]
-    public ulong GuruRoleId { get; set; }
-
-    /// <summary>
-    ///     Gets or sets the Moderator role ID.
-    /// </summary>
-    /// <value>The Moderator role ID.</value>
-    [JsonPropertyName("moderatorRoleId")]
-    public ulong ModeratorRoleId { get; set; }
-
-    /// <summary>
     ///     Gets or sets the Muted role ID.
     /// </summary>
     /// <value>The Muted role ID.</value>
     [JsonPropertyName("mutedRoleId")]
     public ulong MutedRoleId { get; set; }
-
-    /// <summary>
-    ///     Gets or sets the Staff role ID.
-    /// </summary>
-    /// <value>The Staff role ID.</value>
-    /// <remarks>This is the role that should be applied to both Administrator and Moderator.</remarks>
-    [JsonPropertyName("staffRoleId")]
-    public ulong StaffRoleId { get; set; }
 }
