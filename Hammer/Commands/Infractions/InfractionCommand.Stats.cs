ce28ad862990ec4473c133e3834c5c94231c2ff9
Oliver Booth <me@olivr.me>
2022-09-01 23:10:02 +0100

    Include temp/perm mute/ban count (#55)

--- Hammer/Commands/Infractions/InfractionCommand.Stats.cs
+++ Hammer/Commands/Infractions/InfractionCommand.Stats.cs
@@ -30,15 +30,30 @@ internal sealed partial class InfractionCommand
             else
                 embed.WithColor(DiscordColor.Purple);
 
+            var totalInfractions = infractions.Count.ToString("N0");
+            var infractedUsers = infractions.DistinctBy(i => i.UserId).Count().ToString("N0");
+            var warnings = infractions.Count(i => i.Type is InfractionType.Warning).ToString("N0");
+            var gags = infractions.Count(i => i.Type is InfractionType.Gag).ToString("N0");
+            var kicks = infractions.Count(i => i.Type is InfractionType.Kick).ToString("N0");
+            var messagesDeleted = (await _messageDeletionService.CountMessageDeletionsAsync(context.Guild)).ToString("N0");
+            
+            int tempMuteCount = infractions.Count(i => i.Type is InfractionType.TemporaryMute);
+            int muteCount = infractions.Count(i => i.Type is InfractionType.Mute);
+            string mutes = $"{muteCount + tempMuteCount} ({tempMuteCount}T / {muteCount}P)";
+            
+            int tempBanCount = infractions.Count(i => i.Type is InfractionType.TemporaryBan);
+            int banCount = infractions.Count(i => i.Type is InfractionType.Ban);
+            string bans = $"{banCount + tempBanCount} ({tempBanCount}T / {banCount}P)";
+
             embed.WithTitle("Infraction Statistics");
-            embed.AddField("Total Infractions", infractions.Count.ToString("N0"), true);
-            embed.AddField("Infracted Users", infractions.DistinctBy(i => i.UserId).Count().ToString("N0"), true);
-            embed.AddField("Warnings", infractions.Count(i => i.Type is InfractionType.Warning).ToString("N0"), true);
-            embed.AddField("Mutes", infractions.Count(i => i.Type is InfractionType.TemporaryMute or InfractionType.Mute).ToString("N0"), true);
-            embed.AddField("Bans", infractions.Count(i => i.Type is InfractionType.TemporaryBan or InfractionType.Ban).ToString("N0"), true);
-            embed.AddField("Kicks", infractions.Count(i => i.Type is InfractionType.Kick).ToString("N0"), true);
-            embed.AddField("Gags", infractions.Count(i => i.Type is InfractionType.Gag).ToString("N0"), true);
-            embed.AddField("Messages Deleted", (await _messageDeletionService.CountMessageDeletionsAsync(context.Guild)).ToString("N0"), true);
+            embed.AddField("Total Infractions", totalInfractions, true);
+            embed.AddField("Infracted Users", infractedUsers, true);
+            embed.AddField("Warnings", warnings, true);
+            embed.AddField("Mutes", mutes, true);
+            embed.AddField("Bans", bans, true);
+            embed.AddField("Kicks", kicks, true);
+            embed.AddField("Gags", gags, true);
+            embed.AddField("Messages Deleted", messagesDeleted, true);
         }
 
         var builder = new DiscordWebhookBuilder();
