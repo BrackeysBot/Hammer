9c37070a4f7ce64b17f755420a4bede1f6afffa4
Oliver Booth <git@olivr.me>
2024-04-25 12:43:19 +0100

    refactor: switch to Serilog

--- Hammer/Program.cs
+++ Hammer/Program.cs
@@ -5,25 +5,32 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
 using X10D.Hosting.DependencyInjection;
 
 Directory.CreateDirectory("data");
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/latest.log", rollingInterval: RollingInterval.Day)
+#if DEBUG
+    .MinimumLevel.Debug()
+#endif
+    .CreateLogger();
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Configuration.AddJsonFile("data/config.json", true, true);
 
 builder.Logging.ClearProviders();
-builder.Logging.AddNLog();
+builder.Logging.AddSerilog();
 
 builder.Services.AddSingleton(new DiscordClient(new DiscordConfiguration
 {
     Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
-    LoggerFactory = new NLogLoggerFactory(),
+    LoggerFactory = new SerilogLoggerFactory(),
     Intents = DiscordIntents.AllUnprivileged | DiscordIntents.GuildMembers | DiscordIntents.MessageContents
 }));
-
-builder.Services.AddHostedSingleton<LoggingService>();
 
 builder.Services.AddDbContextFactory<HammerContext>();
 builder.Services.AddHostedSingleton<DatabaseService>();
